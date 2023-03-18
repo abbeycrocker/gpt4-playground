22376a6edb372dbe5a217dd90be93fde76081460
William Sepesi <wsepesi@Williams-MacBook-Air.local>
2023-03-18 16:05:53 +0100

    chat mode added

--- pages/todo.tsx
+++ pages/todo.tsx
@@ -1,16 +1,22 @@
+import Link from "next/link";
+
 export default function Todo() {
   return (
     <>
       <main className="">
         <h1>Todos:</h1>
         <ul>
-            <li>Chat mode. turn old box into uneditable, add new input below, feed all as prompt</li>
+            <li>
+              <s>Chat mode. turn old box into uneditable, add new input below, feed all as prompt</s>
+            </li>
+            <li>GPT 3.5 vs GPT4 button</li>
             <li>Stream tokens in to help loading UX</li>
             <li>Temperature and max-token options</li>
             <li>Add system prompt option</li>
             <li>Price tag based on tokenizer</li>
             <li>Some sort of basic auth so AJ can not get charged </li>
         </ul>
+        <Link className="position absolute bottom-0" href='/'>Home</Link>
       </main>
     </>
   )
