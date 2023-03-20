d7dd59b687e578c9eed933b6a6c89ad95a03ee37
William Sepesi <wsepesi@Williams-MacBook-Air.local>
2023-03-20 15:44:31 +0100

    added

--- pages/api/hash_pass.ts
+++ pages/api/hash_pass.ts
@@ -4,8 +4,9 @@ import bcrypt from 'bcrypt'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
+        const hash_pass = process.env.HASH_PASS as string
         const pass = req.body.params.password as string
-        bcrypt.compare(pass, process.env.HASH_PASS, function(err, result) {
+        bcrypt.compare(pass, hash_pass, function(err, result) {
             res.status(200).json({ result })
         });
     }
